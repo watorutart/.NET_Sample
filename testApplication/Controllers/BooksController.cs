c3743404a27900e5ca5987082be4088999d68692
watorutart <watoru374215@gmail.com>
2021-03-31 22:23:04 +0900

    Edit, Deleteにコメントを追加

--- testApplication/Controllers/BooksController.cs
+++ testApplication/Controllers/BooksController.cs
@@ -74,85 +74,108 @@ namespace testApplication.Controllers
         }
 
         // GET: Books/Edit/5
+        // [Edit] リンクから呼び出され、編集フォームを生成
         public async Task<IActionResult> Edit(int? id)
         {
+            // 引数idが空の場合は「404 Not Found」
             if (id == null)
             {
                 return NotFound();
             }
 
+            // 引数idをキーに書籍情報を取得
             var book = await _context.Book.FindAsync(id);
             if (book == null)
             {
                 return NotFound();
             }
+
+            // 編集フォームを表示
             return View(book);
         }
 
         // POST: Books/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        // [Save]ボタンで編集内容をデータベースに反映
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Price,Publisher,Sample")] Book book)
         {
+            // 隠しフィールドのid値と、URLパラメーターのidとが等しいかチェック
             if (id != book.Id)
             {
                 return NotFound();
             }
 
+            // 入力値に問題がなければ更新処理
             if (ModelState.IsValid)
             {
                 try
                 {
+                    // a.モデルの更新をデータベースに反映
                     _context.Update(book);
                     await _context.SaveChangesAsync();
                 }
+                // 競合が発生した場合の処理
                 catch (DbUpdateConcurrencyException)
                 {
+                    // b.書籍が存在しなければ「404 Not Found」
                     if (!BookExists(book.Id))
                     {
                         return NotFound();
                     }
                     else
                     {
+                        // c.書籍が存在する場合は例外
                         throw;
                     }
                 }
+                // データベース更新に成功した場合はリダイレクト
                 return RedirectToAction(nameof(Index));
             }
+            // 入力値に問題がある場合には編集フォームを再描画
             return View(book);
         }
 
         // GET: Books/Delete/5
+        // [Delete]リンクから呼び出され、削除フォームを生成
         public async Task<IActionResult> Delete(int? id)
         {
+            // 引数idが空の場合は「404 Not Found」
             if (id == null)
             {
                 return NotFound();
             }
 
+            // 引数idをキーに書籍情報を取得
             var book = await _context.Book
                 .FirstOrDefaultAsync(m => m.Id == id);
+            // 合致する書籍がない場合は「404 Not Found」
             if (book == null)
             {
                 return NotFound();
             }
-
+            // 削除フォームを表示
             return View(book);
         }
 
         // POST: Books/Delete/5
+        // a.[Delete]ボタンで削除処理を実行&アクション名を表示
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
+            // 引数idをキーに書籍情報を取得
             var book = await _context.Book.FindAsync(id);
+            // b.該当するレコードを削除
             _context.Book.Remove(book);
             await _context.SaveChangesAsync();
+            // 削除後は一覧画面にリダイレクト
             return RedirectToAction(nameof(Index));
         }
 
+        // 指定された書籍が存在するかを判定
         private bool BookExists(int id)
         {
             return _context.Book.Any(e => e.Id == id);
