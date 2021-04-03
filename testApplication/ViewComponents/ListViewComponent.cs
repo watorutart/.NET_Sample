093d857e283023f9e0162058d4c43deae4127d69
watorutart <watoru374215@gmail.com>
2021-04-04 00:01:56 +0900

    Hello/Listに1000円以上の書籍のみを表示するように変更

---
+++ testApplication/ViewComponents/ListViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using testApplication.Models;
+
+namespace testApplication.ViewComponents
+{
+    public class ListViewComponent : ViewComponent
+    {
+        // a.ビューコンポーネントを定義
+        private readonly MyContext _context;
+        // コンテキストクラスを有効化
+        public ListViewComponent(MyContext context)
+        {
+            this._context = context;
+        }
+
+        // b.ビューコンポーネントの実処理
+        public async Task<IViewComponentResult> InvokeAsync(int price)
+        {
+            // c.price円以上の書籍を取得
+            return View(await _context.Book
+                .Where(b => b.Price >= price).ToListAsync());
+        }
+    }
+}
